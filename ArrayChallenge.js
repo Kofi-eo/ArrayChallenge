cf6647e236b0daa9cd180b1e0460e97208b2f557
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-07-08 10:48:13 +0000

    Update ArrayChallenge.js

--- ArrayChallenge.js
+++ ArrayChallenge.js
@@ -3,6 +3,7 @@ function ArrayChallenge(strArr) {
   let index2 = strArr[1].split(",");
   let waiting = "";
   let finalArray = [];
+  let None = "not possible";
 
   for (i = 0; i <= index1.length; i++) {
     for (x = 0; x < index2.length; x++) {
@@ -16,9 +17,16 @@ function ArrayChallenge(strArr) {
     waiting = waiting.concat(index1[i]);
   }
 
-  return finalArray.join(",");
+  if (finalArray.length == 0) {
+    return None;
+  } else {
+    return finalArray.join(",");
+  }
 }
 
-//Example of input
+//Examples of input
 let yeas = ["yellowapplebat", "apple,bat,cat,goodbye,hello,yellow,why"];
 console.log(ArrayChallenge(yeas)); // Result ---->> yellow,apple,bat
+
+let notpos1 = ["whitecream", "apple,bat,cat,goodbye,hello,yellow,why"];
+console.log(ArrayChallenge(notpos1)); // Result ---->> not possible
